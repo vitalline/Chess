5478a8dbecac55e46ecb04f39341d8732678f265
Volodymyr <voldor0@gmail.com>
2020-06-09 09:33:19 +0300

    Removed support for multiple translations for one string as it was too unintuitive

--- src/main/java/com/syntech/chess/text/Translation.java
+++ src/main/java/com/syntech/chess/text/Translation.java
@@ -54,47 +54,7 @@ public enum Translation {
 
     @NotNull
     public String get(String str, @NotNull Object... args) {
-        String result = get(str);
-        int index, prevIndex = 0;
-        for (int i = 0, argsLength = args.length; i < argsLength; i++) {
-            if (args[i] instanceof String) {
-                String value = get((String) args[i]);
-                if (!value.equals(args[i])) {
-                    index = result.indexOf('#', prevIndex);
-                    if (index == -1 || index == result.length() - 1) {
-                        args[i] = value.split("\n")[0];
-                    } else {
-                        while (result.charAt(index + 1) == '#') {
-                            prevIndex = index + 2;
-                            index = result.indexOf('#', prevIndex);
-                            if (index == -1 || index == result.length() - 1) {
-                                break;
-                            }
-                        }
-                        if (index == -1 || index == result.length() - 1) {
-                            args[i] = value.split("\n")[0];
-                        } else {
-                            int valueIndex = Character.digit(result.charAt(index + 1), 16);
-                            if (valueIndex != -1) {
-                                String[] values = value.split("\n");
-                                if (values.length > valueIndex) {
-                                    args[i] = values[valueIndex];
-                                } else {
-                                    args[i] = values[0];
-                                }
-                            }
-                            prevIndex = index + 2;
-                            if (result.length() > prevIndex) {
-                                result = result.substring(0, index) + result.substring(index + 2);
-                            } else {
-                                result = result.substring(0, index);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return String.format(result.replaceAll("##", "#"), args);
+        return String.format(get(str), args);
     }
 
     public boolean exists(@NotNull String str) {
